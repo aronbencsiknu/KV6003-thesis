bd6118886a90df66684904cb965d02438717d115
aronbencsiknu <101459795+aronbencsiknu@users.noreply.github.com>
2023-04-13 15:28:10 +0100

    Tuneable receptive fields

--- population_test.py
+++ population_test.py
@@ -2,38 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch import nn
 import torch
+import random
+import math
 
 class CUBANeuron(nn.Module):
-    def __init__(self, tau, g, Vth, dt, t, exponent):
+    def __init__(self, tau, g, Vth, dt, T):
         super(CUBANeuron, self).__init__()
-        self.tau = nn.Parameter(torch.tensor(tau))
-        self.g = nn.Parameter(torch.tensor(g))
-        self.Vth =nn.Parameter(torch.tensor(Vth))
-        self.exponent = nn.Parameter(torch.tensor(exponent))
-        """self.tau = tau
+        self.tau = tau
         self.g = g
         self.Vth = Vth
-        self.exponent = exponent"""
         self.V = 0
         self.spike = 0
+
+        self.voltages = []
         
         self.fired = False
-        self.time_to_fire = int(t/dt)
+        self.time_to_fire = int(T/dt)
     
     def forward(self, Iext, dt, index):
+        
         # Update membrane potential using Euler method
-        #dV = (-self.V + self.g*Iext) / self.tau
-
-        if not self.fired:
-            EPSILON = 1e-8
-            #dV = -(1/self.tau)*self.V + ((1/(abs(self.g-Iext)+EPSILON))**self.exponent)
-            dV = -(1/self.tau)*self.V + Iext * self.g
-            self.V += dV*dt
+        self.V = math.exp(-dt/self.tau) * self.V + self.g * Iext
+        self.voltages.append(self.V)
+        
         
         # Generate output spike if membrane potential reaches threshold
+        
         if self.V >= self.Vth and not self.fired:
             self.spike = 1
-            self.V = 0
+            #self.V = 0
             self.fired = True
             self.time_to_fire = index
         else:
@@ -43,17 +40,61 @@ class CUBANeuron(nn.Module):
 
 
 class CUBAPopulation():
-    def __init__(self, population_size, tau, g, Vth, dt, t, exponent):
+    def __init__(self, population_size, tau=0.1, g=-5.0, Vth=1, dt=0.001, T=0.1):
         self.population_size = population_size
+        tau = 0.9
         self.tau = tau
         self.g = g
         self.Vth = Vth
-        self.exponent = exponent
+        self.dt = dt
+        self.T = T
 
         self.neurons = []
 
+        self.gains = self.generate_gain(population_size, -0.3, 0.3, tau, dt, T, Vth)
+        self.create_neurons()
+
+        
+    
+    def create_neurons(self):
         for i in range(0, self.population_size):
-            self.neurons.append(CUBANeuron(tau, g+(i/2)*2, Vth, dt, t, exponent))
+            self.neurons.append(CUBANeuron(tau=self.tau, g=self.gains[i], Vth=self.Vth, dt=self.dt, T=self.T))
+
+    def generate_gain(self, count, intercept_low, intercept_high, tau, dt=0.001, T=0.1, Vth=1):
+        gain = []
+
+        for i in range(count):
+            intercept_current = random.uniform(intercept_low, intercept_high)
+            
+            if intercept_current > 0:
+                multiplier = 1
+            else:
+                multiplier = -1
+            
+            g_min = multiplier * 1000
+
+            last_voltage = Vth + 2
+            counter = 0
+            while last_voltage > Vth+0.1:
+                counter += 1
+                g_min = g_min - ((last_voltage - Vth)/200) * multiplier
+                test_neuron = CUBANeuron(tau, g_min, Vth, dt, T)
+                for j in range(0, int(T/dt)):
+                    test_neuron.forward(intercept_current, dt, j)
+
+                #print(test_neuron.voltages[-1])
+                #print((last_voltage - Vth)**2)
+                last_voltage = test_neuron.voltages[-1]
+
+            print(counter, "_",i,":",g_min, " last voltage", last_voltage, " Vth", Vth)
+            gain.append(g_min)
+
+        print("DONE")  
+        return gain
+    
+    def reset_neurons(self):
+        self.neurons = []
+        self.create_neurons()
 
     def forward(self, Iext, dt, index):
         spikes_at_t = []
@@ -64,7 +105,7 @@ class CUBAPopulation():
 
 
 class CUBALayer():
-    def __init__(self, sample_size, population_size, tau=0.1, g=-5.0, Vth=0.01, dt=0.001, t=0.1, exponent=2.0):
+    def __init__(self, sample_size, population_size, tau=0.1, g=-5.0, Vth=1, dt=0.001, T=0.1):
         self.sample_size = sample_size
         self.population_size = population_size
         self.tau = tau
@@ -74,7 +115,7 @@ class CUBALayer():
         self.populations = []
 
         for i in range(self.sample_size):
-            self.populations.append(CUBAPopulation(population_size, tau, g, Vth, dt, t, exponent))
+            self.populations.append(CUBAPopulation(population_size, tau, g, Vth, dt, T))
             
 
     def forward(self, Iext, dt, index):
@@ -83,45 +124,46 @@ class CUBALayer():
             population_spikes_at_t.append(self.populations[i].forward(Iext, dt, index))
 
         return population_spikes_at_t
+    
+    def reset_neurons(self):
+        for i in range(self.sample_size):
+            self.populations[i].reset_neurons()
 
-def simulate_neuron(spike_times, Iext_mean=1, dt=0.001, T=0.1):
+
+def simulate_neuron(spike_times, cuba_layer, Iext_mean=1, dt=0.001, T=0.1):
     # Define time vector and input current
     t = np.arange(0, T, dt)
     Iext = np.full(len(t), Iext_mean)
-    
-    # Create neuron object and simulate
-    cuba_layer = CUBALayer(1, 10)
-    V = np.zeros_like(t)
+    #cuba_layer = CUBALayer(1, 10)
     #spikes = np.zeros_like(t)
     spikes = []
     for i in range(len(t)):
         spikes.append(cuba_layer.forward(Iext[i], dt, i))
-        #V[i] = neuron.V
-    index =  0
+
     for i in range(len(cuba_layer.populations[0].neurons)):
         spike_times[i].append(cuba_layer.populations[0].neurons[i].time_to_fire)
-    #spike_times.append(cuba_layer.populations[0].neurons[0].time_to_fire)
+
+    cuba_layer.reset_neurons()
+
 
     return spike_times
 
+
 # Call the function to simulate the neuron
-current_mean = -0.5
-spike_times1 = [[] for _ in range(10)]
-spike_times2 = []
+current_mean = -1
+spike_times = [[] for _ in range(10)]
 
 x_labels = []
-
+cuba_layer = CUBALayer(1, 10)
 for i in range(100):
-    spike_times1 = simulate_neuron(spike_times1, Iext_mean=current_mean)
-
-    current_mean += 0.01
+    spike_times = simulate_neuron(spike_times, cuba_layer, Iext_mean=current_mean)
+    current_mean += 0.02
     x_labels.append(round(current_mean,3))
 
-
-print(np.asarray(spike_times1).shape)
+print(np.asarray(spike_times).shape)
 
 for i in range(10):
-    plt.plot(spike_times1[i])
+    plt.plot(spike_times[i])
     
 plt.gca().invert_yaxis()
 tick_locs = np.arange(0, len(x_labels), 10)
